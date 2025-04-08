8f44b0506e7bad8297be93a7fab2a81afa95cc9f
saku6911 <sakuykash6911@gmail.com>
2025-04-08 17:50:07 +0900

    correction

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <head>
       <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700&display=swap"
-          rel="stylesheet"
+          rel="preconnect"
         />
       </head>
       <body
--- src/components/layout/header/page.tsx
+++ src/components/layout/header/page.tsx
@@ -8,7 +8,7 @@ export default function Header() {
     <header className="bg-gray-50 flex items-center h-24 py-2.5">
       <div className="w-4/5 mx-auto flex items-center justify-between">
         <div className="w-2/5 lg:w-1/5">
-        <Img src={logoImage} alt="ポートフォリオサイトのロゴ画像" className="object-contain" />
+        <Img src={logoImage} alt="ポートフォリオサイトのロゴ画像" className="object-contain" loading="lazy"/>
         </div>
         <div className="hidden md:flex md:gap-5 text-xl">
           <Link href="#profile">Profile</Link>
