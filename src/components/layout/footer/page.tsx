1f9787176f66c6b5c7542b66cfed0f2e4952f6ce
saku6911 <sakuykash6911@gmail.com>
2025-04-24 13:35:41 +0900

    add sns

--- src/components/layout/footer/page.tsx
+++ src/components/layout/footer/page.tsx
@@ -1,9 +1,31 @@
-
+import Img from "next/image";
+import SNSicon1 from ".././../../public/SNSicon1.png";
+import SNSicon2 from ".././../../public/SNSicon2.png";
+import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-50 flex items-center justify-center h-16">
-     <small>&copy; 2025 Shiratori Portfolio Site</small>
-   </footer>
+    <footer className="bg-gray-50 flex items-center justify-center h-40 p-20 flex-col gap-10">
+      <div className="flex gap-6">
+        <Link href="https://x.com/Cndn1eHw">
+          <Img
+            src={SNSicon1}
+            alt="Xのアイコン画像"
+            className="object-contain"
+            loading="lazy"
+          />
+        </Link>
+        <Link href="https://qiita.com/saku6911">
+          <Img
+            src={SNSicon2}
+            height={50}
+            alt="qiitaのアイコン画像"
+            className="object-contain"
+            loading="lazy"
+          />
+        </Link>
+      </div>
+      <small>&copy; 2025 Shiratori Portfolio Site</small>
+    </footer>
   );
 }
