5a5e6f07a749d57a669628cca4d327fbe09a314b
saku6911 <sakuykash6911@gmail.com>
2025-05-23 13:29:00 +0900

    fix Accessibility

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}', // App Routerの場合
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
